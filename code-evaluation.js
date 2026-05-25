c6f8abe4db0f4769a500f69544206809568cce1a
Ashley <ashleymmarsh@gmail.com>
2026-05-25 15:26:22 -0500

    fixed with added variables

--- code-evaluation.js
+++ code-evaluation.js
@@ -1,6 +1,9 @@
 let name = "Alice";
 let itemsPurchased = 5;
 let cost = 20;
-let shoppingTrip = name + " bought " + itemsPurchased + " items for $" + cost + ".";
+let item = "books";
+let store = "Barnes & Noble";
+
+let shoppingTrip = name + " bought " + itemsPurchased + " " + item + " at " + store + " for $" + cost + ".";
 
 console.log(shoppingTrip);
